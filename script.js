84f65117f8584d7b77453bc95b51238902830bd0
Georgios Kopanidis <kopanidis@me.com>
2023-12-27 10:47:23 +0000

    Inital commit jquery events

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+ 
+
+ 	$("#stream1_btn").on(function() {
+ 		$(".stream1").slideDown();
+		$(".stream1").slideDown(1000);
+
+	});
+	$("#stream2_btn").on("click", function() {
+		$(".stream1").removeClass('highlight_stream');
+		$(".stream2").removeClass('highlight_stream');
+		$(".stream3").removeClass('highlight_stream');
+	  	$(".stream2").addClass('highlight_stream');
+	});
+	$("#stream3_btn").on("click", function() {
+		$(".stream1").removeClass('highlight_stream');
+		$(".stream2").removeClass('highlight_stream');
+		$(".stream3").removeClass('highlight_stream');
+	  	$(".stream3").addClass('highlight_stream');
+	});
+
+
+}); 
